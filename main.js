115dbd5292c43c1ead5a91504761f0a587efa28a
tmdqls2257 <tmdqls2257@naver.com>
2021-06-19 14:42:53 +0900

    remake_addItem

--- main.js
+++ main.js
@@ -1,62 +1,27 @@
 'use strict'
 
-const StartBtn = document.querySelector('.Timer__start');
-const stopBtn = document.querySelector('Timer__stop');
-const itemContainer = document.querySelector('.field');
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
 
-let id =0;
-
-function createItem(){
-  for (let i=0; i<6; i++) {
-    itemContainer.innerHTML +=`<div class="space">
-    <img class="space__bug" src="img/bug.png" alt="space__bug" data-id=${id}>
-  </div>
-  <div class="space">
-    <img class="space__carrot" src="img/carrot.png" alt="space__carrot" data-id=${id}>
-  </div>`;
-    id++;
-  }
+function init(){
+  addItem('carrot', 'img/carrot.png', 5);
+  addItem('bug', 'img/bug.png', 5);
 }
 
-function randPositon(){
-  createItem();
-  const Carrots = itemContainer.querySelectorAll('.space__carrot');
-  console.log(Carrots);
-  const Bugs = itemContainer.querySelectorAll('.space__bug');
-
-  const width = itemContainer.clientWidth;
-  const height = itemContainer.clientHeight;
-  Carrots.forEach((element) =>{
-    const randPositonx = Math.floor(Math.random() * width);
-    const randPositony = Math.floor(Math.random() * height);
-    element.style.transform = `translate(${randPositonx-10}px, ${randPositony-10}px)`;
-  });
-  Bugs.forEach((element) =>{
-    const randPositonx = Math.floor(Math.random() * width);
-    const randPositony = Math.floor(Math.random() * height);
-    element.style.transform = `translate(${randPositonx-10}px, ${randPositony-10}px)`;
-  });
+function addItem(className, imgPath, count){
+for(let i = 0; i < count; i++){
+  const items = document.createElement('img');
+  items.setAttribute('class', className);
+  items.setAttribute('src', imgPath);
+  field.appendChild(items);
+  const x = randomNumber(fieldRect.width - 80);
+  const y = randomNumber(fieldRect.height - 80);
+  items.style.position = 'absolute';
+  items.style.left = `${x}px`;
+  items.style.top = `${y}px`;
 }
-
-function lastTime(){
-  let timeleft = 9;
-  const downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.querySelector('.Timer__time').innerHTML = "Finished";
-  } else {
-    document.querySelector('.Timer__time').innerHTML = `${timeleft}:00`;
-  }
-  timeleft -= 1;
-}, 1000);
 }
-
-
-StartBtn.addEventListener('click', ()=>{
-  lastTime();
-  randPositon();
-});
-
-stopBtn.addEventListener('click', ()=>{
-
-});
+function randomNumber(max){
+  return Math.floor(Math.random() * max);
+}
+init();
